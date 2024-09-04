b02fb35c4518c5573411185d3853bff38c1a7fc5
MistrrrMedia <music.mistrrr@gmail.com>
2024-09-04 18:22:12 -0500

    Initial Commit

---
+++ Fortune Kitty/Fortune_KittyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fortune_KittyApp.swift
+//  Fortune Kitty
+//
+//  Created by Nicholas Nguyen on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Fortune_KittyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
